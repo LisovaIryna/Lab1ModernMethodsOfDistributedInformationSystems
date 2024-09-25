872a12c303b6f7360ec46eb9f0bea6b210876c95
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2024-09-25 19:56:24 +0300

    Controller created

---
+++ Controllers/BallDownController.cs
@@ -0,0 +1,20 @@
+﻿using Lab1ModernMethodsOfDistributedInformationSystems.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1ModernMethodsOfDistributedInformationSystems.Controllers;
+
+public class BallDownController : Controller
+{
+    private readonly IGetTimeService _getTimeService;
+
+    public BallDownController(IGetTimeService getTimeService)
+    {
+        _getTimeService = getTimeService;
+    }
+
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
