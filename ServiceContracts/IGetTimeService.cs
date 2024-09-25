218824e99bb3f122ab0dbff45a7bd3b4c62de38c
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2024-09-25 19:48:04 +0300

    Service created (without implementation)

---
+++ ServiceContracts/IGetTimeService.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab1ModernMethodsOfDistributedInformationSystems.ServiceContracts;
+
+public interface IGetTimeService
+{
+    public double CalculateStraightSlideTime(double h, double g);
+    public double CalculateParabolicSlideTime(double h, double g);
+    public double CalculateIntegral(double finish, double start, int N, double g);
+}
---
+++ Services/GetTimeService.cs
@@ -0,0 +1,21 @@
+﻿using Lab1ModernMethodsOfDistributedInformationSystems.ServiceContracts;
+
+namespace Lab1ModernMethodsOfDistributedInformationSystems.Services;
+
+public class GetTimeService : IGetTimeService
+{
+    public double CalculateStraightSlideTime(double h, double g)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalculateParabolicSlideTime(double h, double g)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalculateIntegral(double x0, double x1, int N, double g)
+    {
+        throw new NotImplementedException();
+    }
+}
