028484a2f190461665dda070680cdf8d88efbd06
LisovaIryna <79648928+LisovaIryna@users.noreply.github.com>
2024-09-25 19:50:51 +0300

    Add service

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Lab1ModernMethodsOfDistributedInformationSystems.ServiceContracts;
+using Lab1ModernMethodsOfDistributedInformationSystems.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IGetTimeService, GetTimeService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
